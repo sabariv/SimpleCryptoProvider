b8606bb00a267ec172649f596ed1abda31238971
Sabari <sabari.venkatachalam@quartesian.com>
2018-07-24 20:30:42 +0530

    Initial Draft

---
+++ SimpleCryptoProvider/SimpleCryptoProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SimpleCryptoProvider
+{
+
+    public interface ISimpleCryptoProvider
+    {
+        byte[] Encrypt<T>(string plainText) where T : HashAlgorithm, new();
+
+        string Encrypt<T>(string plainText, bool shouldReturnBase64String) where T : HashAlgorithm, new();        
+    }
+
+    public class SimpleCryptoProvider : ISimpleCryptoProvider
+    {
+        public byte[] Encrypt<T>(string plainText) where T : HashAlgorithm, new()
+        {            
+            if (!string.IsNullOrWhiteSpace(plainText))
+            {
+                using(var provider=new T())
+                {
+                    byte[] plainData = Encoding.UTF8.GetBytes(plainText);
+
+                    byte[] decipheredData = provider.ComputeHash(plainData);
+
+                    if (decipheredData != null && decipheredData.Length > 0)
+                        return decipheredData;
+                }
+            }
+            return default(byte[]);
+        }
+
+        public string Encrypt<T>(string plainText, bool shouldReturnBase64String) where T : HashAlgorithm, new()
+        {
+            if (!string.IsNullOrWhiteSpace(plainText))
+            {
+                using (var provider = new T())
+                {
+                    byte[] plainData = Encoding.UTF8.GetBytes(plainText);
+
+                    byte[] decipheredData = provider.ComputeHash(plainData);
+
+                    if (shouldReturnBase64String)
+                        return Convert.ToBase64String(decipheredData);
+                    else
+                        return BitConverter.ToString(decipheredData).Replace(@"-", string.Empty);
+                }
+                
+            }            
+            return string.Empty;
+        }
+
+        
+    }
+
+}
---
+++ SimpleCryptoProviderTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCryptoProviderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCryptoProvider.ISimpleCryptoProvider md = new SimpleCryptoProvider.SimpleCryptoProvider();
+
+            Console.WriteLine(md.Encrypt<MD5CryptoServiceProvider>(@"Hello World", true));
+
+            Console.WriteLine(md.Encrypt<MD5CryptoServiceProvider>(@"Hello World", false));
+
+            Console.WriteLine(md.Encrypt<SHA1CryptoServiceProvider>(@"Hello World", true));
+
+            Console.WriteLine(md.Encrypt<SHA1CryptoServiceProvider>(@"Hello World",false));
+            
+            Console.ReadKey();
+        }
+    }
+}
